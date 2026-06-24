47ca8aa285d97563796a17f2f92e8c05cdbacd9d
Varsha <varsha.official81@gmail.com>
2026-06-24 22:06:28 +0530

    Fixed MongoDB connection and IP whitelist

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json(
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected!'))
-.catch((err) => console.log('MongoDB Error: ', err));
-
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected!')).catch((err) => console.log('MongoDB Error: ', err));
 app.get('/', (req, res) => {
   res.send('E-commerce API is running!');
 });
@@ -22,3 +19,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
+
