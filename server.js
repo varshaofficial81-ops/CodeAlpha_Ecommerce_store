12b72631bbff6340637a93522c609ad296927647
Varsha <varsha.official81@gmail.com>
2026-06-24 17:08:14 +0530

    Added project setup files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json(
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected!'))
+.catch((err) => console.log('MongoDB Error: ', err));
+
+app.get('/', (req, res) => {
+  res.send('E-commerce API is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
